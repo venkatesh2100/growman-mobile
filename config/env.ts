63c4ceb2429de93f6e8a6cc472eeab00c740d996
venky2100 <venkyvenkat.iit@gmail.com>
2026-05-09 20:02:34 +0530

    react-native version

--- config/env.ts
+++ config/env.ts
@@ -3,8 +3,8 @@ import { Platform } from 'react-native';
 const DEFAULT_API_URL = Platform.OS === 'android'
   ? 'http://192.168.1.13:8080/api/v1'  // Android emulator
   : 'http://localhost:8080/api/v1'; // iOS simulator / web
-// console.debug(process.env.EXPO_PUBLIC_API_URL, "API URL")
-export const API_URL =  DEFAULT_API_URL;
+console.debug(process.env.EXPO_PUBLIC_API_URL, "API URL")
+export const API_URL =  process.env.EXPO_PUBLIC_API_URL;
 console.debug(API_URL, "API URL")
 
 export const GOOGLE_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID ?? '';
